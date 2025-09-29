63d2b3081dccdd2372711e3eb1c834414f379291
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-29 23:27:37 +0200

    Add player movement on keystroke

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import {tileMap01} from "./SokobanBase.js";
 
-/* Parse mapgrid free from space */
+/* Parse mapgrid free from space, p, b and large letters*/
 const mapping = {
-  ' ': 'f',
+  ' ': 'fl',
   'W': 'wa',
   'P': 'pl',
   'G': 'go',
@@ -12,6 +12,8 @@ const mapping = {
 tileMap01.mapGrid = tileMap01.mapGrid.map(row =>
   row.map(cell => [ mapping[cell[0]] ?? cell[0] ])
 );
+
+const myMap = tileMap01.mapGrid;
 
 
 /* Init the game */
@@ -29,7 +31,39 @@ const initTheBoard = () => {
 
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    console.log("DOM laddad")
+let playerRow = tileMap01.mapGrid.findIndex(row =>
+  row.some(cell => cell[0] === 'pl')
+);
+
+let playerCol = tileMap01.mapGrid[playerRow].findIndex(cell => cell[0] === 'pl');
+
+let playerPos = { row: playerRow, col: playerCol };
+
+
+const moveplayer = (move) => {
+    const oldRow = playerPos.row;
+    const oldCol = playerPos.col;
+
+    if (move == 'up') { playerPos.row = (playerPos.row -1)};
+    if (move == 'down') { playerPos.row = (playerPos.row + 1)};
+    if (move == 'left') { playerPos.col = (playerPos.col - 1)};
+    if (move == 'right') { playerPos.col = (playerPos.col + 1)};
+
+    myMap[oldRow][oldCol] = ['fl'];
+    myMap[playerPos.row][playerPos.col] = ['pl']
     initTheBoard();
+}
+
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    initTheBoard();
+})
+
+document.addEventListener('keypress', (e) => {
+
+    if (e.key == 'w') return moveplayer('up');
+    if (e.key == 's') return moveplayer('down');
+    if (e.key == 'd') return moveplayer('right');
+    if (e.key == 'a') return moveplayer('left');
+    
 })
